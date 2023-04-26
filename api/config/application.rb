b37afaa3f020c40d313b0d1227cb684e7a5f88a1
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2023-04-26 22:45:51 +0800

    [RNG-002] - [BE] Install and Setup Graphql Environment (#2)
    Co-authored-by: acatzk <joshuaimalay@gmail.com>

--- api/config/application.rb
+++ api/config/application.rb
@@ -12,6 +12,7 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -30,6 +31,10 @@ module Api
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.

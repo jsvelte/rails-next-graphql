7ff16e978a0bbdff163c2099aded37bc4684cacb
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2023-04-27 11:23:43 +0800

    [RNG-003] - [BE] Implement Fetch in Users and Post Functionality (#3)

--- api/app/graphql/types/user_type.rb
+++ api/app/graphql/types/user_type.rb
@@ -3,9 +3,16 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :email, String
-    field :name, String
+    field :email, String, null: true
+    field :name, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :posts, [Types::PostType], null: true
+    field :post_count, Integer, null: true
+
+    def post_count
+      object.posts.size
+    end
   end
 end

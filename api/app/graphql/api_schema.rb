c98089478786297f1945d46cb4d44da0e0dbf5fd
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2023-05-02 11:42:16 +0800

    [RNG-005] - [BE] Implement Sign In with Token Functionality (#5)

--- api/app/graphql/api_schema.rb
+++ api/app/graphql/api_schema.rb
@@ -2,39 +2,23 @@ class ApiSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
-  # For batch-loading (see https://graphql-ruby.org/dataloader/overview.html)
-  use GraphQL::Dataloader
+  def self.resolve_type(_type, object, _ctx)
+    type_class = "::Types::#{object.class}Type".safe_constantize
 
-  # GraphQL-Ruby calls this when something goes wrong while running a query:
-  def self.type_error(err, context)
-    # if err.is_a?(GraphQL::InvalidNullError)
-    #   # report to your bug tracker here
-    #   return nil
-    # end
-    super
+    raise ArgumentError, "Cannot resolve type for class #{object.class.name}" unless type_class.present?
+
+    type_class
   end
 
-  # Union and Interface Resolution
-  def self.resolve_type(abstract_type, obj, ctx)
-    # TODO: Implement this method
-    # to return the correct GraphQL object type for `obj`
-    raise(GraphQL::RequiredImplementationMissingError)
+  def self.object_from_id(node_id, _ctx)
+    return unless node_id.present?
+
+    record_class_name, record_id = GraphQL::Schema::UniqueWithinType.decode(node_id)
+    record_class = record_class_name.safe_constantize
+    record_class&.find_by id: record_id
   end
 
-  # Stop validating when it encounters this many errors:
-  validate_max_errors(100)
-
-  # Relay-style Object Identification:
-
-  # Return a string UUID for `object`
-  def self.id_from_object(object, type_definition, query_ctx)
-    # For example, use Rails' GlobalID library (https://github.com/rails/globalid):
-    object.to_gid_param
-  end
-
-  # Given a string UUID, find the object
-  def self.object_from_id(global_id, query_ctx)
-    # For example, use Rails' GlobalID library (https://github.com/rails/globalid):
-    GlobalID.find(global_id)
+  def self.id_from_object(object, _type, _ctx)
+    GraphQL::Schema::UniqueWithinType.encode(object.class.name, object.id)
   end
 end
--- api/app/graphql/types/user_type.rb
+++ api/app/graphql/types/user_type.rb
@@ -3,8 +3,8 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :email, String, null: true
-    field :name, String, null: true
+    field :email, String, null: false
+    field :name, String,  null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
